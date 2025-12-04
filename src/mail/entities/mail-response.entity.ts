de320c6430c650b89e32de597a78e43433ccba13
truong-tan-sang <161419882+truong-tan-sang@users.noreply.github.com>
2025-12-04 14:46:19 +0700

    feat(mail): add response entity for mail endpoints

---
+++ src/mail/entities/mail-response.entity.ts
@@ -0,0 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class MailResponseEntity {
+  @ApiProperty({ example: 'Send email from ecommerce shop successfully!', description: 'Response message' })
+  message: string;
+}
