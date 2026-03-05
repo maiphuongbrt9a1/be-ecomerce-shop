aaf6e20785d45db37b1e464ccf4de3eb78f5fbcc
Vo Phuong <maiphuongbrt9a1@gmail.com>
2026-03-05 09:18:32 +0700

    doc (add log point): add log point for vnpay module. It's consoleLogger function in loggerFn in VnpayModule.registerAsync({...})

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,7 +3,7 @@ import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { PrismaService } from '@/prisma/prisma.service';
 import { VnpayModule } from 'nestjs-vnpay';
-import { ignoreLogger } from 'vnpay';
+import { consoleLogger } from 'vnpay';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
@@ -13,7 +13,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: async (configService: ConfigService) => ({
         secureSecret: configService.getOrThrow<string>('VNPAY_SECURE_SECRET'),
         tmnCode: configService.getOrThrow<string>('VNPAY_TMN_CODE'),
-        loggerFn: ignoreLogger,
+        loggerFn: consoleLogger,
       }),
       inject: [ConfigService],
     }),
